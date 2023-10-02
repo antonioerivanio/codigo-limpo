af28197eca951e3a5a6dba1d97fa4cce988ca82a
erivanio.cruz <antonioerivanio@gmail.com>
2023-10-01 22:57:55 -0300

    feat: lista como parametros

---
+++ br/com/tce/ce/gov/codigolLimpo/cap02/ListaComoParametros.java
@@ -0,0 +1,8 @@
+package br.com.tce.ce.gov.codigolLimpo.cap02;
+
+public class ListaComoParametros {
+    void monades(Integer... args);
+    void diades(String nome, Integer... args);
+    void triades(Stirng nome, int quantidade, Integer... args);
+
+}
