e51078b8f391c37c6f117007740ad01638300c88
erivanio.cruz <antonioerivanio@gmail.com>
2023-10-01 22:58:55 -0300

    feat: objetos como parametro

---
+++ br/com/tce/ce/gov/codigolLimpo/cap02/ObjetosComoParemetros.java
@@ -0,0 +1,6 @@
+package br.com.tce.ce.gov.codigolLimpo.cap02;
+
+public class ObjetosComoParemetros {
+    Circulo criarCirculo(double x, double y, double raio);
+    Circulo criaCirculo(Ponto center, double radio);
+}
