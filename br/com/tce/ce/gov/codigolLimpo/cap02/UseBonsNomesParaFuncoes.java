83aa37900c7c3a4679e61a99653b2a6649483e94
erivanio.cruz <antonioerivanio@gmail.com>
2023-10-01 23:00:01 -0300

    feat: escrever bons nomes

---
+++ br/com/tce/ce/gov/codigolLimpo/cap02/UseBonsNomesParaFuncoes.java
@@ -0,0 +1,11 @@
+package br.com.tce.ce.gov.codigolLimpo.cap02;
+
+public class UseBonsNomesParaFuncoes {
+    void escrever(String nome);
+    void escreverCampo(String nome);
+
+
+    void assertEquals(expected, actual);
+    void assertExpectedEqualsActual(expected, actual);
+
+}
