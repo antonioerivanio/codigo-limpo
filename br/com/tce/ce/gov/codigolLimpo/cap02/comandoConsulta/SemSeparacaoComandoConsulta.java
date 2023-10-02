e972ac030d152474d5a8fd9f8dc7ad00f80c0f18
erivanio.cruz <antonioerivanio@gmail.com>
2023-10-01 23:16:57 -0300

    refactor: class renomeada

---
+++ br/com/tce/ce/gov/codigolLimpo/cap02/comandoConsulta/SemSeparacaoComandoConsulta.java
@@ -0,0 +1,6 @@
+package br.com.tce.ce.gov.codigolLimpo.cap02.comandoConsulta;
+
+public class SemSeparacaoComandoConsulta {
+    public boolean set(String atributo, String valor){}
+    if(set("nomeUsuario", "tioBob")){}
+}
